4cc4259327bcf9d580914ca28c2deb44102ac162
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-27 17:36:02 +0530

    Add files via upload

---
+++ AsciiAtoZ.java
@@ -0,0 +1,24 @@
+public class AsciiAtoZ {
+    public static void main(String[] args) {
+        for(char c='A';c<='Z';c++){
+            System.out.println(c+" = "+(int)c);
+        }
+    }
+}
+
+public class AsciiAtoZ {
+    public static void main(String[] args) {
+        for(char c='A';c<='Z';c++){
+            System.out.println(c+" = "+(int)c);
+        }
+    }
+}
+
+public class AsciiAtoZ {
+    public static void main(String[] args) {
+        for(char c='A';c<='Z';c++){
+            System.out.println(c+" = "+(int)c);
+        }
+    }
+}
+

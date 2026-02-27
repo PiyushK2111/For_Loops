878515f883db76c8ff6924d308fa0f1db7b92f54
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-27 18:23:52 +0530

    Add files via upload

---
+++ ProductOfDigits.java
@@ -0,0 +1,32 @@
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n=452;
+        int prod=1;
+        for(;n!=0;n/=10){
+            prod*=n%10;
+        }
+        System.out.println("Product: "+prod);
+    }
+}
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n=452;
+        int prod=1;
+        for(;n!=0;n/=10){
+            prod*=n%10;
+        }
+        System.out.println("Product: "+prod);
+    }
+}
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        int n=452;
+        int prod=1;
+        for(;n!=0;n/=10){
+            prod*=n%10;
+        }
+        System.out.println("Product: "+prod);
+    }
+}

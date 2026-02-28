e8b250a8c2cb6b1c1f56dbe3c0a45505ff47c2de
Piyush Kumar <piyushkumar2111@outlook.com>
2026-02-28 16:02:51 +0530

    Add files via upload

---
+++ SpyNumber.java
@@ -0,0 +1,53 @@
+public class SpyNumber {
+    public static void main(String[] args) {
+        int n=1124;
+        int sum=0,prod=1;
+        int temp=n;
+        for(;temp!=0;temp/=10){
+            int d=temp%10;
+            sum+=d;
+            prod*=d;
+        }
+        if(sum==prod){
+            System.out.println("Spy Number");
+        }else{
+            System.out.println("Not Spy Number");
+        }
+    }
+}
+
+public class SpyNumber {
+    public static void main(String[] args) {
+        int n=1124;
+        int sum=0,prod=1;
+        int temp=n;
+        for(;temp!=0;temp/=10){
+            int d=temp%10;
+            sum+=d;
+            prod*=d;
+        }
+        if(sum==prod){
+            System.out.println("Spy Number");
+        }else{
+            System.out.println("Not Spy Number");
+        }
+    }
+}
+
+public class SpyNumber {
+    public static void main(String[] args) {
+        int n=1124;
+        int sum=0,prod=1;
+        int temp=n;
+        for(;temp!=0;temp/=10){
+            int d=temp%10;
+            sum+=d;
+            prod*=d;
+        }
+        if(sum==prod){
+            System.out.println("Spy Number");
+        }else{
+            System.out.println("Not Spy Number");
+        }
+    }
+}
